54b898ad65e039d20d5eb74fea26c769757ec9f5
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-10-21 11:33:41 -0400

    Using refs for reactivity

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,8 @@
     Home
     <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click me</button>
+    <input type="text" v-model="name">
+    <button @click="age++">Increase Age</button>
   </div>
 </template>
 
@@ -17,28 +19,35 @@ export default {
     // This is how refs worked in options API
     /* this.$refs.age */
 
-    console.log(this)
+    //console.log(this)
 
-    const p = ref('hello')
+    //const p = ref('hello')
 
     // Cant access p before its been returned
-    console.log(p, p.value)
+    //console.log(p, p.value)
 
-    console.log('setup')
+    //console.log('setup')
 
     // These are not reactive values, unlike doing the same within data function
-    let name = 'mario'
-    let age = 30
+/*     let name = 'mario'
+    let age = 30 */
+
+    // These are now reactive values; just because its const doesnt mean it cant change. the const is the ref
+    const name = ref('mario')
+    const age = ref(30)
     
     const handleClick = () => {
-      console.log(p, p.value)
+        name.value = 'luigi'
+        age.value = 35
+      // name = 'luigi' // doesnt update the name in the template because the name variable is not reactive by default
+/*       console.log(p, p.value)
       p.value.classList.add('test')
-      p.value.textContent = 'hello, ninjas'
+      p.value.textContent = 'hello, ninjas' */
     }
 
-    return { name, age, handleClick, p }
+    return { name, age, handleClick}
   }
-  // This is reactive:
+  // This is reactive data, so changes to them will change the variable in the template:
 /*   data() {
     return {
       age: 40
