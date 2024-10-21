d2e355a7bb10515e89db2fc76a1a1309a6e0e1bc
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-10-21 12:09:10 -0400

    Watch and watchEffect

--- src/views/Home3.vue
+++ src/views/Home3.vue
@@ -6,11 +6,12 @@
     <div v-for="name in matchingNames" :key="name">
         {{ name }}
     </div>
+    <button @click="handleClick">Stop watching</button>
   </div>
 </template>
 
 <script>
-import { computed, ref } from 'vue'
+import { computed, ref, watch, watchEffect } from 'vue'
 
 export default {
     name: 'Home3',
@@ -18,11 +19,28 @@ export default {
         const search = ref('')
         const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
 
+        // This runs whenever the watching ref is  updated
+        const stopWatch = watch(search, () => {
+            console.log('watch function ran')
+        })
+
+        // Runs initially when setup loads; if add search.value as argument, then runs it whenever the argument updates
+        // will run whenever any value inside it updates; watches any dependencies used inside it
+        const stopEffect = watchEffect(() => {
+            console.log('watchEffect function ran', search.value)
+        })
+
         const matchingNames = computed(() => {
             return names.value.filter((name) => name.includes(search.value))
         })
 
-        return { names, search, matchingNames }
+        // to stop the functions from watching, just need to call them
+        const handleClick = () => {
+            stopWatch()
+            stopEffect()
+        }
+
+        return { names, search, matchingNames, handleClick }
     }
 }
 </script>
