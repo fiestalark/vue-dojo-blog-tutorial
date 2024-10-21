1cc571042bf4d6d2ab333583c941fe00eb467757
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-10-21 11:17:15 -0400

    Basic Composition API setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+// Using composition API now; setup will run before any other lifecycle hook
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    console.log('setup')
+
+    // These are not reactive values, unlike doing the same within data function
+    let name = 'mario'
+    let age = 30
+    
+    const handleClick = () => {
+      console.log('You clicked me')
+    }
+
+    return { name, age, handleClick }
   }
+  // This is reactive:
+/*   data() {
+    return {
+      age: 40
+    }
+  } */
+
 }
 </script>
